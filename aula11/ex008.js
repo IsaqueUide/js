ab1155dab1e3426264245133669c6314e3427b45
IsaqueUide <isaqueuide@gmail.com>
2026-04-13 16:02:01 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 84
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}else{
+    console.log('Dirigindo dentro da velocidade permitida!')
+}
+console.log('Diriga sempre usando um cinto em segurança')
