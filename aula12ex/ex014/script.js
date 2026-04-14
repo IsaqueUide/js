330014a96cbfa856d5e7abeeb0bc7cf6cd147244
IsaqueUide <isaqueuide@gmail.com>
2026-04-14 15:49:25 -0300

    update

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -4,15 +4,14 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML += `${hora}h`
-    var corpo = document.body
     if(hora >= 0 && hora < 12){
         img.src = "imagens/manhã.jpg"
-        corpo.style.background = "rgb(248, 238, 102)"
+        document.body.style.background = "rgb(248, 202, 102)"
     }else if (hora >= 12 && hora <= 18){
         img.src = "imagens/tarde.jpg"
-        corpo.style.background = "rgb(29, 58, 96)"
+        document.body.style.background = "rgb(29, 58, 96)"
     }else{
         img.src = "imagens/noite.jpg"
-        corpo.style.background = "rgb(11, 9, 55)"
+        document.body.style.background = "rgb(11, 9, 55)"
     }
 }
