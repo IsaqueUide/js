ad4ce033c462cdd5922b735c2085669c94518801
IsaqueUide <isaqueuide@gmail.com>
2026-04-15 22:32:34 -0300

    update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -8,6 +8,32 @@ function verificar(){
     } else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if (idade >=0 && idade < 12){
+                //criança
+            }else if (idade >= 10 && idade < 21){
+                //jovem
+            }else if(idade < 50){
+                //adulto
+            }else(idade){
+                //idoso
+            }
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >=0 && idade < 12){
+                //criança
+            }else if (idade >= 10 && idade < 21){
+                //jovem
+            }else if(idade < 50){
+                //adulto
+            }else(idade){
+                //idoso
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
 }
