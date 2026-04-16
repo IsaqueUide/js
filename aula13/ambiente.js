f1e0eebc9931e69df9f8e230668ca2d9c1aeee64
IsaqueUide <isaqueuide@gmail.com>
2026-04-16 10:36:34 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,11 @@
-var c = 1
-while (c <= 10){
-    console.log(`Passo ${c}`)
-    c++
+var a = 1
+
+while (a <= 10){
+    console.log(`a = ${a}`)
+    a++
 }
+var b = 1
+do{
+    console.log(`b = ${b}`)
+    b++
+} while(b <= 10)
