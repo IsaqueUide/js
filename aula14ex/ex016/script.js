22dc24600e34aefa930442ff316dfa98b40fee5f
IsaqueUide <isaqueuide@gmail.com>
2026-04-17 17:23:28 -0300

    update

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,13 +1,32 @@
 function contar(){
-    var txtini = document.getElementById('iini')
-    var ini = Number(txtini.value)
-    var txtfim = document.getElementById('ifim')
-    var fim = Number(txtfim.value)
-    var txtpasso = document.getElementById('ipas')
-    var pas = Number(txtpasso.value)
-    var res = document.getElementById('res')
-    while(ini <= fim){
-        ini = +passo
-        res.innerHTML = `${ini}`
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'IMPOSSÍVEL CONTAR'
+       0// window.alert('[ERRO] Faltam dados!')
+    } else{
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f ){
+            // contagem crescente
+            for(let c = i; c <= f; c+=p){
+                res.innerHTML += `${c} ▶️ `
+            }
+        } else{
+            // contagem decrescente
+            for(let c = i; c >= f; c-=p){
+                res.innerHTML += `${c} ▶️ `
+            }
+        }
+        res.innerHTML += `🚩`
     }
 }
