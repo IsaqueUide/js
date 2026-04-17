b8f9b682ad77f853c0e966bdcb8285440d644260
IsaqueUide <isaqueuide@gmail.com>
2026-04-17 15:50:34 -0300

    update

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,5 @@
+function onclick{
+    let ini = Number.parseInt(document.getElementById('txtini'))
+    let fim = Number.parseInt(document.getElementById('txtfim'))
+    let passo = Number.parseInt(document.getElementById('txtpas'))
+}
