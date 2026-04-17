5d05180ab3bc7638b83cb34af82db6173b861a2d
IsaqueUide <isaqueuide@gmail.com>
2026-04-17 16:11:37 -0300

    update

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,7 +1,10 @@
 function onclick{
-    let ini = document.getElementById('txtini')
-    let fim = document.getElementById('txtfim')
-    let passo = document.getElementById('txtpas')
+    let txtini = document.getElementById('iini')
+    let ini = Number(iini.value)
+    let txtfim = document.getElementById('ifim')
+    let fim = Number(ifim.value)
+    let txtpasso = document.getElementById('ipas')
+    let txtpasso = Number(ipas.value)
     let res = document.getElementById('res')
     res.innerHTML = `${ini}, ${fim}, ${passo}`
 }
