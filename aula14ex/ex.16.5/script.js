d39b08481c459727c488762c37702ef127c80b94
IsaqueUide <isaqueuide@gmail.com>
2026-04-18 10:26:14 -0300

    Update script.js

--- aula14ex/ex.16.5/script.js
+++ aula14ex/ex.16.5/script.js
@@ -7,9 +7,12 @@ function contar(){
     if(ini.value.length == 0 || fim.value.length == 0 || passos.value.length == 0){
         res.innerHTML = '[ERROR] preencha <strong>todos</strong> os dados acima'
     }else {
-        
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passos.value)
+        if( p <= 0){
+            res.innerHTML='[ERROR]'
+        }
         }
     }
