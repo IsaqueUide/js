61c63233370a5edf03ab0f8b53bd38da6b6fb6c7
IsaqueUide <isaqueuide@gmail.com>
2026-04-18 15:53:30 -0300

    Update script.js

--- aula14ex/ex017.5/script.js
+++ aula14ex/ex017.5/script.js
@@ -2,9 +2,10 @@ function contar(){
     let num = document.getElementById('txtnum')
     let n1 = Number(num.value)
     let res = document.getElementById('res')
-    
-    for(c = 0; c<= 10; c++){
-        let n2 = n1*c
+    let c = '0'
+    let n2 = n1*c
+    while(c<= 10){
         res.innerHTML = `${n1} x ${c} = ${n2} <br>`
+        c++
     }
 }
