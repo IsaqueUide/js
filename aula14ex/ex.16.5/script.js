4f5642bc2acf609a087b7396ec2bf1e2c464e5f2
IsaqueUide <isaqueuide@gmail.com>
2026-04-18 10:18:00 -0300

    update

--- aula14ex/ex.16.5/script.js
+++ aula14ex/ex.16.5/script.js
@@ -1,3 +1,18 @@
 function contar(){
+    let ini = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passos = document.getElementById('txtpasso')
+    let res = document.getElementById('res')
     
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passos.value)
+    
+    res.innerHTML = `${i}, ${f}, ${p}`
+    
+    if(ini.value.length == 0 || fim.value.length == 0 || passos.value.length == 0){
+        res.innerHTML = '[ERROR] preencha <strong>todos</strong> os dados acima'
+    }else{
+
+    }
 }
