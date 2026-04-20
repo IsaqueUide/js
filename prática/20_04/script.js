ad8924c0e1b4d0f572771995177e3712c8e3b803
IsaqueUide <isaqueuide@gmail.com>
2026-04-20 18:11:46 -0300

    Update script.js

--- prática/20_04/script.js
+++ prática/20_04/script.js
@@ -3,26 +3,31 @@ function verificar(){
     let ano = data.getFullYear()
     let nasc = document.getElementById('txtnas')
     let res = document.getElementById('res')
-    if (nas.value.length == 0 || nas.value > ano){
+    if (nasc.value.length == 0 || nasc.value > ano){
         window.alert('[ERRO] Digite os dados corretamente!')
     } else{
-        let radgen = document.getElementById('radsex')
+        let radgen = document.getElementById('radgen')
         let idade = ano - Number(nasc.value)
-        genero = ''
+        let genero = ''
+        let direito = ''
         let img = document.createElement('img')
-        img.setAttribute('src', 'id')
+        img.setAttribute('id', 'foto')
         if(radgen[0].checked){
             genero = "Homem"
             if (idade < 18){
                 //Bebe Não pode dirigir
-                
-            }else if{
-
+                img.setAttribute('src', 'bebeM.jpg')
+                direito = "não pode dirigir."
+            }else if(idade >= 18){
+                img.setAttribute('src', 'jovemM.jpg')
+                direito = "pode dirigir"
             }
             
         }else if (radgen[1].checked){
             genero = "Mulher"
 
         }
+        res.innerHTML = `Você é ${genero} e tem ${idade} anos, por isso ${direito}`
+        res.appendChild(img)
     }
 }
